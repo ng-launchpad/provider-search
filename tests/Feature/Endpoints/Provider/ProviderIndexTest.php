faac425613e0f31a81e4e487cd23e6fde8058ba5
Nik Fedorov <nik@shedcollective.org>
2021-12-07 22:50:53 +0500

    feat: add tests for two endpoints

---
+++ tests/Feature/Endpoints/Provider/ProviderIndexTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Endpoints\Provider;
+
+use App\Models\Provider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProviderIndexTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_returns_list_of_providers()
+    {
+        // arrange
+        $provider = Provider::factory()->create();
+
+        // act
+        $response = $this->withoutExceptionHandling()
+            ->getJson('/api/providers');
+
+        // assert
+        $response->assertOk();
+    }
+}
