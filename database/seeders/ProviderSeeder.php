5f97851c8bbdacf6e4b02d49eeee1e70573a575b
Pablo de la Peña <p@shedcollective.org>
2021-12-07 17:02:39 +0000

    chore: Add Provider seeder

--- database/seeders/ProviderSeeder.php
+++ database/seeders/ProviderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Provider;
 use Illuminate\Database\Seeder;
 
 class ProviderSeeder extends Seeder
@@ -13,6 +14,8 @@ class ProviderSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Provider::factory()
+            ->times(100)
+            ->create();
     }
 }
