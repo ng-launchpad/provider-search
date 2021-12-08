d2f82cbd5449e14c870b9e6a3f2c0a855fe4d192
Pablo de la Peña <p@shedcollective.org>
2021-12-08 16:00:14 +0000

    chore: Add stub for data source sync command

---
+++ app/Console/Commands/DataSource/SyncCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands\DataSource;
+
+use App\Services\DataSourceService;
+use Illuminate\Console\Command;
+
+class SyncCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'datasource:sync';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Syncs upstream data sources into the database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return Command::SUCCESS;
+    }
+}
