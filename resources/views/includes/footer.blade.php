74e84e6104fec0cec5ccdd04301d46640e43b27b
Anton Sydorenko <shout2794@gmail.com>
2021-12-08 13:31:56 +0100

    feat: added footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,30 @@
 <footer class="footer">
-    FOOTER
+    <div class="container">
+        <div class="footer__inner">
+            <div class="footer__left">
+                <ul class="footer__menu">
+                    <li class="footer__menu-item">
+                        <a href="#" class="footer__menu-link text--styled-link">
+                            Terms of use
+                        </a>
+                    </li>
+                    <li class="footer__menu-item">
+                        <a href="#" class="footer__menu-link text--styled-link">
+                            Privacy & security
+                        </a>
+                    </li>
+                    <li class="footer__menu-item">
+                        <a href="#" class="footer__menu-link text--styled-link">
+                            California Consumer Privacy
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer__right">
+                <div class="footer__copyright">
+                    © 2021 Allstate Benefits
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
