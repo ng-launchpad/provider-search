5ec9f6b9f3c419faaf80fe0c6a8b047bb3c40468
Pablo de la Peña <p@shedcollective.org>
2021-12-08 11:15:29 +0000

    chore: Run `make idehelper` as part of `build`

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,9 @@
 vendor/bin/sail artisan migrate
 vendor/bin/sail artisan storage:link
 
+# Refresh IDE helper files
+make idehelper
+
 # Install and build frontend
 vendor/bin/sail npm install
 vendor/bin/sail npm run dev
-
-# Refresh IDE helper files
-make idehelper
