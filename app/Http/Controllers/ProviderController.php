725f3550d4ccfc1adbfa1d1e2402872913116020
Pablo de la Peña <p@shedcollective.org>
2021-12-08 12:30:55 +0000

    feat: Support provider filtering

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ProviderResource;
 use App\Models\Provider;
+use Illuminate\Http\Request;
 
 class ProviderController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $keywords = $request->query->get('keywords');
+
+        $query = Provider::query();
+
+        if ($keywords) {
+            $query->where('label', 'LIKE', "%$keywords%");
+        }
+
         return ProviderResource::collection(
-            Provider::simplePaginate()
+            $query->simplePaginate()
         );
     }
 
