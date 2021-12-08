635c68f1255c68c067ed632db679e1b5f8424b9b
Pablo de la Peña <p@shedcollective.org>
2021-12-08 15:59:39 +0000

    chore: Add test for data source sync command

---
+++ tests/Feature/Commands/DataSouce/SyncCommandTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Commands\DataSouce;
+
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class SyncCommandTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_executes_successfully()
+    {
+        $this
+            ->artisan('datasource:sync')
+            ->assertExitCode(Command::SUCCESS);
+    }
+}
