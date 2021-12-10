5404fdf02a9a0e763d9caa080aa1b0c3271b2cf3
Pablo de la Peña <p@shedcollective.org>
2021-12-10 11:12:08 +0000

    chore: Add requirement for a FilesystemInterface in Sftp adapter

--- app/Services/DataSource/Connection/Sftp.php
+++ app/Services/DataSource/Connection/Sftp.php
@@ -3,9 +3,16 @@
 namespace App\Services\DataSource\Connection;
 
 use App\Interfaces\DataSource\Connection;
+use League\Flysystem\FilesystemInterface;
 
 class Sftp implements Connection
 {
+    private FilesystemInterface $filesystem;
+
+    public function __construct(FilesystemInterface $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
     public function download(string $file, $resource): Connection
     {
         //  @todo (Pablo 2021-12-08) - stream $file into $resource
