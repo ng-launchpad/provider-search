9a988d56ab608dcd44ee1e889bbe1f117944c250
Pablo de la Peña <p@shedcollective.org>
2021-12-10 16:30:04 +0000

    chore: Add stub tests for DataSource mappers

---
+++ tests/Unit/DataSource/Mapper/AentaTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\DataSource\Mapper;
+
+use PHPUnit\Framework\TestCase;
+
+class AentaTest extends TestCase
+{
+    /** @test */
+    public function it_maps_the_data()
+    {
+        $this->markTestIncomplete();
+
+        // arrange
+
+        // act
+
+        // assert
+    }
+}
