e0050af820d248100c7e205c7cbe9de7650374fa
Pablo de la Peña <p@shedcollective.org>
2021-12-10 17:13:26 +0000

    chore: Add `factory` method to Mappers

--- app/Services/DataSource/Mapper/Vsp.php
+++ app/Services/DataSource/Mapper/Vsp.php
@@ -5,8 +5,13 @@ namespace App\Services\DataSource\Mapper;
 use App\Services\DataSource\Interfaces\Mapper;
 use App\Models\Provider;
 
-class Vsp implements Mapper
+final class Vsp implements Mapper
 {
+    public static function factory(): self
+    {
+        return new self();
+    }
+
     public function transform(array $item): Provider
     {
         //  @todo (Pablo 2021-12-08) - take the item and transform into a provider
