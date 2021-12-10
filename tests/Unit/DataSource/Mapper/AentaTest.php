bb19fc7780563bbc44f9fc8e469b0efeac46e47c
Pablo de la Peña <p@shedcollective.org>
2021-12-10 17:16:50 +0000

    chore: Add test for Aenta mapper

--- tests/Unit/DataSource/Mapper/AentaTest.php
+++ tests/Unit/DataSource/Mapper/AentaTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Unit\DataSource\Mapper;
 
+use App\Models\Provider;
+use App\Services\DataSource\Mapper\Aenta;
+use Faker\Factory;
+use Illuminate\Support\Collection;
 use PHPUnit\Framework\TestCase;
 
 class AentaTest extends TestCase
@@ -9,12 +13,29 @@ class AentaTest extends TestCase
     /** @test */
     public function it_maps_the_data()
     {
-        $this->markTestIncomplete();
-
         // arrange
+        $data       = $this->getData();
+        $collection = new Collection($data);
+        $mapper     = Aenta::factory();
 
         // act
+        $collection = $collection->map(fn(array $item) => $mapper->transform($item));
 
         // assert
+        $this->assertInstanceOf(Collection::class, $collection);
+        $this->assertCount(1, $collection);
+        $this->assertInstanceOf(Provider::class, $collection->get(0));
+        $this->assertEquals($data[0]['COLUMN NAME'], $collection->get(0)->label);
+    }
+
+    private function getData(): array
+    {
+        $faker = Factory::create();
+
+        return [
+            [
+                'COLUMN NAME' => $faker->words(5, true),
+            ],
+        ];
     }
 }
