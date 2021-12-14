422d8a8524152a8399c03ad84ec70509689d43b2
Pablo de la Peña <p@shedcollective.org>
2021-12-14 15:34:08 +0000

    feat: Add truncate method for dropping all data

--- app/Services/DataSourceService.php
+++ app/Services/DataSourceService.php
@@ -2,7 +2,11 @@
 
 namespace App\Services;
 
+use App\Models\Language;
+use App\Models\Location;
+use App\Models\Network;
 use App\Models\Provider;
+use App\Models\Speciality;
 use App\Services\DataSource\Interfaces\Connection;
 use App\Services\DataSource\Interfaces\Mapper;
 use App\Services\DataSource\Interfaces\Parser;
@@ -12,6 +16,17 @@ final class DataSourceService
     public static function factory()
     {
         return new self();
+    }
+
+    public function truncate(): self
+    {
+        Provider::truncate();
+        Network::truncate();
+        Language::truncate();
+        Location::truncate();
+        Speciality::truncate();
+
+        return $this;
     }
 
     /**
@@ -34,11 +49,7 @@ final class DataSourceService
 
         $collection = $collection->map(fn(array $item) => $mapper->transform($item));
 
-        //  @todo (Pablo 2021-12-10) - ensure we handle updates properly
-
         $collection->each(fn(Provider $provider) => $provider->save());
-
-        //  @todo (Pablo 2021-12-08) - delete untouched items from the database
 
         return $this;
     }
