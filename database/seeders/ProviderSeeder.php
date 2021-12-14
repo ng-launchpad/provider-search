9fe34a5b7e33d5dd19dc9fbca0330b30dd623e2c
Pablo de la Peña <p@shedcollective.org>
2021-12-14 14:19:37 +0000

    feat: Add seeder for provider languages and specialities

--- database/seeders/ProviderSeeder.php
+++ database/seeders/ProviderSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Language;
 use App\Models\Location;
 use App\Models\Network;
 use App\Models\Provider;
+use App\Models\Speciality;
 use Illuminate\Database\Seeder;
 
 class ProviderSeeder extends Seeder
@@ -29,6 +31,14 @@ class ProviderSeeder extends Seeder
 
                 for ($i = 0; $i < rand(1, 4); $i++) {
                     $provider->locations()->attach(Location::query()->inRandomOrder()->first());
+                }
+
+                for ($i = 0; $i < rand(0, 2); $i++) {
+                    $provider->languages()->attach(Language::query()->inRandomOrder()->first());
+                }
+
+                for ($i = 0; $i < rand(0, 2); $i++) {
+                    $provider->specialities()->attach(Speciality::query()->inRandomOrder()->first());
                 }
 
             });
