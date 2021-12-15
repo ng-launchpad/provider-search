8aa336088c36170a95beeef1103b9bc1b6f299c6
Pablo de la Peña <p@shedcollective.org>
2021-12-15 20:36:16 +0000

    feat: Add logic for fetching distinct specialities for a given network and state

--- app/Http/Resources/SpecialityResource.php
+++ app/Http/Resources/SpecialityResource.php
@@ -14,9 +14,6 @@ class SpecialityResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id'    => $this->id,
-            'label' => $this->label,
-        ];
+        return $this->label;
     }
 }
