a728e39ccb4658c92be55576119bf893d5a2df04
Pablo de la Peña <p@shedcollective.org>
2021-12-15 20:41:58 +0000

    chore: Remove double Location seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,6 @@ class DatabaseSeeder extends Seeder
             LocationSeeder::class,
             HospitalSeeder::class,
             ProviderSeeder::class,
-            LocationSeeder::class,
         ]);
     }
 }
