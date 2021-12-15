d8981727c85278d5509e3972429f46744458b185
Pablo de la Peña <p@shedcollective.org>
2021-12-15 14:59:42 +0000

    chore: Add placeholder connections, mappers and parsers for data

---
+++ app/Services/DataSource/Mapper/Hch.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services\DataSource\Mapper;
+
+use App\Services\DataSource\Mapper;
+
+final class Hch extends Mapper
+{
+    protected function getMap(): array
+    {
+        return [
+            'COLUMN NAME' => 'label',
+        ];
+    }
+
+    protected function getLanguageKeys(): array
+    {
+        return [];
+    }
+
+    protected function getLocationKeys(): array
+    {
+        return [];
+    }
+
+    protected function getNetworkKey(): string
+    {
+        return '';
+    }
+
+    protected function getSpecialityKeys(): array
+    {
+        return [];
+    }
+
+    protected function getProviderKeys(): array
+    {
+        return [];
+    }
+}
