24e546f4d371ad173a9b99de279899588c57dadd
Pablo de la Peña <p@shedcollective.org>
2021-12-15 16:41:47 +0000

    chore: Add tests for network and state requirements for provider index

--- tests/Feature/Endpoints/Provider/ProviderIndexTest.php
+++ tests/Feature/Endpoints/Provider/ProviderIndexTest.php
@@ -7,6 +7,7 @@ use App\Models\Network;
 use App\Models\Provider;
 use App\Models\State;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class ProviderIndexTest extends TestCase
@@ -21,48 +22,58 @@ class ProviderIndexTest extends TestCase
     {
         parent::setUp();
 
-        $this->network = Network::factory()->create();
-
-        $this->createProvider($this->state, $this->provider);
+        $this->createProvider($this->state, $this->network, $this->provider);
+        $this->createProvider($this->state, $this->network);
+        $this->createProvider($this->state);
         $this->createProvider();
     }
 
     /** @test */
-    public function it_returns_list_of_providers()
+    public function it_requires_a_network_id()
     {
-        // arrange
-        $provider = $this->provider;
-
-        // act
-        $response = $this
-            ->withoutExceptionHandling()
-            ->getJson(route('api.providers.index'));
-
         // assert
-        $response
-            ->assertOk()
-            ->assertJsonCount(2, 'data')
-            ->assertJsonPath('data.0.label', $provider->label);
-    }
-
-    /** @test */
-    public function it_returns_providers_filtered_by_state()
-    {
-        // arrange
-        $provider = $this->provider;
-        $state    = $this->state;
+        $this->expectException(ValidationException::class);
 
         // act
         $response = $this
             ->withoutExceptionHandling()
             ->getJson(route('api.providers.index', [
-                'state' => $state->id,
+                'state_id' => $this->state->id,
+            ]));
+    }
+
+    /** @test */
+    public function it_requires_a_state_id()
+    {
+        // assert
+        $this->expectException(ValidationException::class);
+
+        // act
+        $response = $this
+            ->withoutExceptionHandling()
+            ->getJson(route('api.providers.index', [
+                'network_id' => $this->network->id,
+            ]));
+    }
+
+    /** @test */
+    public function it_returns_list_of_providers_for_a_network_and_state()
+    {
+        // arrange
+        $provider = $this->provider;
+
+        // act
+        $response = $this
+            ->withoutExceptionHandling()
+            ->getJson(route('api.providers.index', [
+                'network_id' => $this->network->id,
+                'state_id'   => $this->state->id,
             ]));
 
         // assert
         $response
             ->assertOk()
-            ->assertJsonCount(1, 'data')
+            ->assertJsonCount(2, 'data')
             ->assertJsonPath('data.0.label', $provider->label);
     }
 
@@ -76,7 +87,9 @@ class ProviderIndexTest extends TestCase
         $response = $this
             ->withoutExceptionHandling()
             ->getJson(route('api.providers.index', [
-                'keywords' => $provider->label,
+                'network_id' => $this->network->id,
+                'state_id'   => $this->state->id,
+                'keywords'   => $provider->label,
             ]));
 
         // assert
@@ -86,11 +99,26 @@ class ProviderIndexTest extends TestCase
             ->assertJsonPath('data.0.label', $provider->label);
     }
 
-    private function createProvider(&$state = null, &$provider = null)
+    /** @test */
+    public function it_returns_providers_filtered_by_type()
     {
-        $state    = State::factory()->create();
+        self::markTestIncomplete();
+        //  @todo (Pablo 2021-12-15) - complete this test
+    }
+
+    /** @test */
+    public function it_returns_providers_filtered_by_scope()
+    {
+        self::markTestIncomplete();
+        //  @todo (Pablo 2021-12-15) - complete this test
+    }
+
+    private function createProvider(State &$state = null, Network &$network = null, Provider &$provider = null)
+    {
+        $state    = $state ?? State::factory()->create();
+        $network  = $network ?? Network::factory()->create();
         $location = Location::factory()->for($state, 'addressState')->create();
-        $provider = Provider::factory()->create();
+        $provider = $provider ?? Provider::factory()->for($network)->create();
         $provider->locations()->attach($location);
     }
 }
