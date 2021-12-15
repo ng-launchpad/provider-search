eacead84e5110bd3d85fe522bebc36dce23f5c05
Pablo de la Peña <p@shedcollective.org>
2021-12-15 17:36:31 +0000

    chore: Add stub tests for city and speciality endpoints

---
+++ tests/Feature/Endpoints/City/CityIndexTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Endpoints\City;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CityIndexTest extends TestCase
+{
+    /** @test */
+    public function it_requires_network_id()
+    {
+        self::markTestIncomplete();
+    }
+
+    /** @test */
+    public function it_requires_state_id()
+    {
+        self::markTestIncomplete();
+    }
+
+    /** @test */
+    public function it_returns_list_of_cities()
+    {
+        self::markTestIncomplete();
+    }
+}
