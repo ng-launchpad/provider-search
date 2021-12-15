1df73bbdf90c78a779eb8674c6d6a7edef61df14
Pablo de la Peña <p@shedcollective.org>
2021-12-15 17:44:00 +0000

    chore: Add speciality network and state validation tests

--- tests/Feature/Endpoints/Speciality/SpecialityIndexTest.php
+++ tests/Feature/Endpoints/Speciality/SpecialityIndexTest.php
@@ -2,27 +2,69 @@
 
 namespace Tests\Feature\Endpoints\Speciality;
 
+use App\Models\Network;
+use App\Models\State;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class SpecialityIndexTest extends TestCase
 {
+    use RefreshDatabase;
+
+    private $network;
+    private $state;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->network = Network::factory()->create();
+        $this->state   = State::factory()->create();
+    }
+
     /** @test */
     public function it_requires_network_id()
     {
-        self::markTestIncomplete();
+        // arrange
+        $state = $this->state;
+
+        // assert
+        $this->expectException(ValidationException::class);
+
+        // act
+        $this
+            ->withoutExceptionHandling()
+            ->getJson(route('api.specialities.index', [
+                'state_id' => $state->id,
+            ]));
     }
 
     /** @test */
     public function it_requires_state_id()
     {
-        self::markTestIncomplete();
+        // arrange
+        $network = $this->network;
+
+        // assert
+        $this->expectException(ValidationException::class);
+
+        // act
+        $this
+            ->withoutExceptionHandling()
+            ->getJson(route('api.specialities.index', [
+                'network_id' => $network->id,
+            ]));
     }
 
     /** @test */
     public function it_returns_list_of_specialities()
     {
         self::markTestIncomplete();
+        // arrange
+
+        // act
+
+        // assert
     }
 }
