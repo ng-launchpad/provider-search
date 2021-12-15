65246f2fe38442c7ca3fbed287d3d90401be7b2f
Pablo de la Peña <p@shedcollective.org>
2021-12-15 15:01:56 +0000

    chore: Add Unique constraint on npi:network_id for providers

--- database/migrations/2021_12_08_130200_create_providers_table.php
+++ database/migrations/2021_12_08_130200_create_providers_table.php
@@ -17,16 +17,18 @@ class CreateProvidersTable extends Migration
     {
         Schema::create('providers', function (Blueprint $table) {
             $table->id();
+            $table->string('npi', 150);
+            $table->foreignIdFor(Network::class)->constrained('networks')->restrictOnDelete();
             $table->string('label', 150);
             $table->string('type', 150)->nullable();
-            $table->string('npi', 150);
             $table->string('degree', 10)->nullable();
             $table->string('website', 150)->nullable();
             $table->enum('gender', [Provider::GENDER_MALE, Provider::GENDER_FEMALE])->nullable();
-            $table->foreignIdFor(Network::class)->constrained('networks')->restrictOnDelete();
             $table->boolean('is_facility');
             $table->boolean('is_accepting_new_patients')->nullable();
             $table->timestamps();
+
+            $table->unique(['npi', 'network_id']);
         });
     }
 
