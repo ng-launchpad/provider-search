65141cdcabe52b6d1c1ceb19a3573facfc760f07
Pablo de la Peña <p@shedcollective.org>
2021-12-15 16:42:13 +0000

    chore: Add HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\NetworkResource;
+use App\Http\Resources\StateResource;
+use App\Models\Network;
+use App\Models\State;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+    }
+}
