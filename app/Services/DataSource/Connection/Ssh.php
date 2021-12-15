ac027c3034a8c5c90ba55b547444ed8019abd89f
Pablo de la Peña <p@shedcollective.org>
2021-12-15 21:36:04 +0000

    feat: Add support for SSH connection

--- app/Services/DataSource/Connection/Ssh.php
+++ app/Services/DataSource/Connection/Ssh.php
@@ -6,9 +6,27 @@ use App\Services\DataSource\Interfaces\Connection;
 
 final class Ssh implements Connection
 {
+    private \Spatie\Ssh\Ssh $ssh;
+
+    public static function factory(
+        string $host,
+        string $username,
+        int $port
+    ) {
+        return new \Spatie\Ssh\Ssh($username, $host, $port);
+    }
+
+    public function __construct(\Spatie\Ssh\Ssh $ssh)
+    {
+        $this->ssh = $ssh;
+    }
+
     public function download(string $file, $resource): Connection
     {
-        //  @todo (Pablo 2021-12-15) - implement this
+        $this->ssh->download(
+            $file,
+            stream_get_meta_data($resource)['uri']
+        );
         return $this;
     }
 }
