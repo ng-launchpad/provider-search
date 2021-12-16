6a3c0f6df7e2e3e64131b5f8f9c3eb8216efa13c
Pablo de la Peña <p@shedcollective.org>
2021-12-16 13:58:34 +0000

    chore: Fix `DataSourceServiceTest::it_syncs_data` test

--- tests/Unit/DataSource/DataSourceServiceTest.php
+++ tests/Unit/DataSource/DataSourceServiceTest.php
@@ -52,45 +52,105 @@ class DataSourceServiceTest extends TestCase
     /** @test */
     public function it_syncs_data()
     {
-        self::markTestIncomplete();
         // arrange
-        $connection = $this->createMock(Connection::class);
+        $service    = DataSourceService::factory();
         $faker      = Factory::create();
-        $network    = Network::factory()->create();
+        $connection = $this->createMock(Connection::class);
+        $parser     = $this->createMock(Parser::class);
+        $mapper     = $this->createMock(Mapper::class);
 
-        /**
-         * Create a new model, but not synced to the database;
-         * include minimum required fields to avoid DB exceptions
-         */
-        $provider                            = new Provider();
-        $provider->npi                       = $faker->numerify('##########');
-        $provider->network_id                = $network->id;
-        $provider->label                     = $faker->company;
-        $provider->gender                    = $faker->randomElement([Provider::GENDER_MALE, Provider::GENDER_FEMALE]);
-        $provider->network_id                = $network->id;
-        $provider->is_facility               = $faker->boolean;
-        $provider->is_accepting_new_patients = $faker->boolean;
+        // Create/make assets which will be referenced later
+        $network     = Network::factory()->create();
+        $state       = State::factory()->create();
+        $provider1   = Provider::factory()->make();
+        $provider2   = Provider::factory()->make();
+        $location1   = Location::factory()->for($state)->make();
+        $location2   = Location::factory()->for($state)->make();
+        $language1   = Language::factory()->make(['label' => $faker->unique->languageCode]);
+        $language2   = Language::factory()->make(['label' => $faker->unique->languageCode]);
+        $speciality1 = Speciality::factory()->make(['label' => $faker->unique->word]);
+        $speciality2 = Speciality::factory()->make(['label' => $faker->unique->word]);
+        $speciality3 = Speciality::factory()->make(['label' => $faker->unique->word]);
 
-        $mapper = $this->createMock(Mapper::class);
-        $mapper
-            ->method('transform')
-            ->willReturn($provider);
+        //  Configure mocks: Connection
+        $connection
+            ->expects($this->once())
+            ->method('download');
 
-        $collection = new Collection([
-            ['actual content does not matter'],
-        ]);
-
-        $parser = $this->createMock(Parser::class);
+        //  Configure mocks: Parser
         $parser
-            ->method('parse')
-            ->willReturn($collection);
+            ->expects($this->once())
+            ->method('parse');
 
-        $service = DataSourceService::factory();
+        //  Configure mocks: Mapper
+        $config = [
+            'extractLanguages' => [
+                $language1,
+                $language2,
+            ],
+
+            'extractLocations' => [
+                $location1,
+                $location2,
+            ],
+
+            'extractSpecialities' => [
+                $speciality1,
+                $speciality2,
+                $speciality3,
+            ],
+
+            'extractProviders' => [
+                $provider1,
+                $provider2,
+            ],
+
+            'extractProviderLocations' => [
+                [$provider1, $location1, true],
+                [$provider1, $location2, true],
+                [$provider2, $location2, true],
+            ],
+
+            'extractProviderLanguages' => [
+                [$provider1, $language1],
+                [$provider1, $language2],
+                [$provider2, $language2],
+            ],
+
+            'extractProviderSpecialities' => [
+                [$provider1, $speciality1],
+                [$provider1, $speciality2],
+                [$provider1, $speciality3],
+                [$provider2, $speciality3],
+            ],
+        ];
+
+        foreach ($config as $method => $items) {
+            $mapper
+                ->expects($this->once())
+                ->method($method)
+                ->willReturn(Collection::make($items));
+        }
 
         // act
-        $service->sync($network, 'test.file', $connection, $mapper, $parser);
+        $service->sync(
+            $network,
+            '/path/to/test.file',
+            $connection,
+            $parser,
+            $mapper
+        );
 
         // assert
-        $this->assertcount(1, Provider::all());
+        $this->assertCount(2, Language::all());
+        $this->assertCount(2, Location::all());
+        $this->assertCount(3, Speciality::all());
+        $this->assertCount(2, Provider::all());
+        $this->assertCount(2, $provider1->locations()->get());
+        $this->assertCount(2, $provider1->languages()->get());
+        $this->assertCount(3, $provider1->specialities()->get());
+        $this->assertCount(1, $provider2->locations()->get());
+        $this->assertCount(1, $provider2->languages()->get());
+        $this->assertCount(1, $provider2->specialities()->get());
     }
 }
