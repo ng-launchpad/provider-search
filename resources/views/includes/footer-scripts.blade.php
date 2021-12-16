ff3b1abed22eaaff088f5c126df8b7c6d7564b1a
Anton Sydorenko <shout2794@gmail.com>
2021-12-16 12:33:47 +0100

    chore: added usersnap script

--- resources/views/includes/footer-scripts.blade.php
+++ resources/views/includes/footer-scripts.blade.php
@@ -2,4 +2,13 @@
     window.state = {!! $state !!};
     window.networks = {!! $networks !!}
 </script>
+<script>
+    window.onUsersnapCXLoad = function(api) {
+        api.init();
+    }
+    var script = document.createElement('script');
+    script.defer = 1;
+    script.src = 'https://widget.usersnap.com/global/load/3dfd05f8-d465-496d-801b-ebe880c11799?onload=onUsersnapCXLoad';
+    document.getElementsByTagName('head')[0].appendChild(script);
+</script>
 <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
