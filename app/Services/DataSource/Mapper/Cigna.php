6c4f0c3ad2c2bd9770fd7139e21d32971bb3ac15
Pablo de la Peña <p@shedcollective.org>
2021-12-17 15:34:59 +0000

    feat: Complete CIGNA mapper

--- app/Services/DataSource/Mapper/Cigna.php
+++ app/Services/DataSource/Mapper/Cigna.php
@@ -2,16 +2,19 @@
 
 namespace App\Services\DataSource\Mapper;
 
+use App\Models\State;
 use App\Services\DataSource\Mapper;
 
 final class Cigna extends Mapper
 {
-    protected function getMap(): array
-    {
-        return [
-            'COLUMN NAME' => 'label',
-        ];
-    }
+    const COL_X                                     = 0;
+    const COL_NPI                                   = 1;
+    const COL_SERVICE_LOCATION_LINE_1               = 3;
+    const COL_SERVICE_LOCATION_CITY                 = 5;
+    const COL_SERVICE_LOCATION_STATE                = 6;
+    const COL_SERVICE_LOCATION_ZIP_CODE             = 7;
+    const COL_SERVICE_LOCATION_COUNTRY              = 8;
+    const COL_SERVICE_LOCATION_PRIMARY_PHONE_NUMBER = 9;
 
     protected function getLanguageKeys(): array
     {
@@ -20,7 +23,19 @@ final class Cigna extends Mapper
 
     protected function getLocationKeys(): array
     {
-        return [];
+        return [
+            'label'            => self::COL_X,
+            'address_line_1'   => self::COL_SERVICE_LOCATION_LINE_1,
+            'address_city'     => self::COL_SERVICE_LOCATION_CITY,
+            'address_state_id' => function (array $item) {
+                return State::query()
+                    ->where('code', $item[self::COL_SERVICE_LOCATION_STATE])
+                    ->firstOrFail()
+                    ->id;
+            },
+            'address_zip'      => self::COL_SERVICE_LOCATION_ZIP_CODE,
+            'phone'            => self::COL_SERVICE_LOCATION_PRIMARY_PHONE_NUMBER,
+        ];
     }
 
     protected function getSpecialityKeys(): array
@@ -35,6 +50,11 @@ final class Cigna extends Mapper
 
     protected function getProviderKeys(): array
     {
-        return [];
+        return [
+            'label'       => self::COL_X,
+            'type'        => fn() => 'Pharmacy',
+            'npi'         => self::COL_NPI,
+            'is_facility' => fn() => true,
+        ];
     }
 }
