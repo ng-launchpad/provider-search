00d0e62ace364160005dbc79cfa9deb00428aa60
Pablo de la Peña <p@shedcollective.org>
2022-01-20 15:37:06 +0000

    chore: Complete `Mapper::extractProviderHospitals` method

--- app/Services/DataSource/Mapper.php
+++ app/Services/DataSource/Mapper.php
@@ -225,8 +225,26 @@ abstract class Mapper implements Interfaces\Mapper
 
     public function extractProviderHospitals(Collection $collection, Network $network): Collection
     {
-        //  @todo (Pablo 2021-12-14) - complete this
-        return new Collection();
+        $collectionOut = new Collection();
+
+        $collection->each(function ($item) use ($collectionOut, $network) {
+
+            $provider  = Provider::findByNpiAndNetworkOrFail(
+                $item[$this->getProviderNpiKey()],
+                $network
+            );
+            $hospitals = $this->extractHospitals(Collection::make([$item]));
+
+            foreach ($hospitals as $hospital) {
+                $hospital = Hospital::where('label', $hospital->label)->firstOrFail();
+                $collectionOut->add([
+                    $provider,
+                    $hospital,
+                ]);
+            }
+        });
+
+        return $collectionOut;
     }
 
     protected abstract function getLanguageKeys(): array;
