c61c07c930ae10f5a60c5cb75b773b26e5206ba5
Pablo de la Peña <p@shedcollective.org>
2022-01-26 16:51:49 +0000

    chore: Apply versioning and update test to speciality index

--- app/Http/Controllers/SpecialityController.php
+++ app/Http/Controllers/SpecialityController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\SpecialityResource;
+use App\Models\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -18,14 +19,13 @@ class SpecialityController extends Controller
             'state_id'   => 'required',
         ]);
 
-        //  @todo (Pablo 2022-01-26) - apply versioning
-
         $result = DB::query()
             ->from('location_provider')
             ->leftJoin('locations', 'location_provider.location_id', '=', 'locations.id')
             ->leftJoin('providers', 'location_provider.provider_id', '=', 'providers.id')
             ->leftJoin('provider_speciality', 'location_provider.provider_id', '=', 'provider_speciality.provider_id')
             ->leftJoin('specialities', 'provider_speciality.speciality_id', '=', 'specialities.id')
+            //->where('locations.version', '=', Setting::version())
             ->where('locations.address_state_id', '=', $request->get('state_id'))
             ->where('providers.network_id', '=', $request->get('network_id'))
             ->whereNotNull('specialities.label')
