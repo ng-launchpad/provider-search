95730a980c67623f1305eeb3f6bbe94f56425486
Pablo de la Peña <p@shedcollective.org>
2022-02-09 16:34:09 +0000

    chore: Implement Sftp:: it_returns_the_most_recently_modified_file

--- tests/Unit/DataSource/Connection/SftpTest.php
+++ tests/Unit/DataSource/Connection/SftpTest.php
@@ -9,6 +9,39 @@ use PHPUnit\Framework\TestCase;
 class SftpTest extends TestCase
 {
     /** @test */
+    public function it_returns_the_most_recently_modified_file()
+    {
+        // arrange
+        $path       = '/path/to/files';
+        $files      = [
+            [
+                'path'      => $path . '/file1.txt',
+                'timestamp' => time() - 1,
+            ],
+            [
+                'path'      => $path . '/file2.txt',
+                'timestamp' => time(),
+            ],
+            [
+                'path'      => $path . '/file3.txt',
+                'timestamp' => time() + 1,
+            ],
+        ];
+        $filesystem = $this->createMock(FilesystemInterface::class);
+        $filesystem
+            ->method('listContents')
+            ->willReturn($files);
+
+        $connection = new Sftp($filesystem);
+
+        // act
+        $file = $connection->getMostRecentlyModified($path);
+
+        // assert
+        $this->assertEquals($files[2], $file);
+    }
+
+    /** @test */
     public function it_downloads_from_the_remote_server()
     {
         // arrange
