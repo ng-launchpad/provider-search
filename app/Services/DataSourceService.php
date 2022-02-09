12e4c9e9ed48669be078e1d6e7d4b116cf8825d2
Pablo de la Peña <p@shedcollective.org>
2022-02-09 17:22:15 +0000

    feat: Explicitly define what makes items unique

--- app/Services/DataSourceService.php
+++ app/Services/DataSourceService.php
@@ -61,51 +61,76 @@ final class DataSourceService
         $collection = $parser->parse($file);
 
         $mapper->extractLanguages($collection)
-            ->unique()
+            ->unique('label')
             ->each(fn(Language $model) => $model->save());
 
         $mapper->extractLocations($collection)
-            ->unique()
+            ->unique('hash')
             ->each(fn(Location $model) => $model->save());
 
         $mapper->extractSpecialities($collection)
-            ->unique()
+            ->unique('label')
             ->each(fn(Speciality $model) => $model->save());
 
         $mapper->extractHospitals($collection)
-            ->unique()
+            ->unique('label')
             ->each(fn(Hospital $model) => $model->save());
 
         $mapper->extractProviders($collection)
-            ->unique()
+            ->unique('npi')
             ->each(function (Provider $model) use ($network) {
                 $model->network_id = $network->id;
                 $model->save();
             });
 
         $mapper->extractProviderLocations($collection, $network)
-            ->unique()
+            ->unique(function ($item) {
+                return sprintf(
+                    '%s,%s,%s',
+                    $item['location_id'],
+                    $item['provider_id'],
+                    $item['is_primary']
+                );
+            })
             ->each(function (array $set) {
                 [$provider, $location, $is_primary] = $set;
                 $provider->locations()->attach($location, ['is_primary' => $is_primary]);
             });
 
         $mapper->extractProviderLanguages($collection, $network)
-            ->unique()
+            ->unique(function ($item) {
+                return sprintf(
+                    '%s,%s',
+                    $item['language_id'],
+                    $item['provider_id']
+                );
+            })
             ->each(function (array $set) {
                 [$provider, $language] = $set;
                 $provider->languages()->attach($language);
             });
 
         $mapper->extractProviderSpecialities($collection, $network)
-            ->unique()
+            ->unique(function ($item) {
+                return sprintf(
+                    '%s,%s',
+                    $item['provider_id'],
+                    $item['speciality_id']
+                );
+            })
             ->each(function (array $set) {
                 [$provider, $speciality] = $set;
                 $provider->specialities()->attach($speciality);
             });
 
         $mapper->extractProviderHospitals($collection, $network)
-            ->unique()
+            ->unique(function ($item) {
+                return sprintf(
+                    '%s,%s',
+                    $item['hospital_id'],
+                    $item['provider_id']
+                );
+            })
             ->each(function (array $set) {
                 [$provider, $hospital] = $set;
                 $provider->hospitals()->attach($hospital);
