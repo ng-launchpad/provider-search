d26bbc021618bc4a2b34f250bdf43a80997f52a6
Pablo de la Peña <p@shedcollective.org>
2022-02-09 11:55:50 +0000

    chore: Add legal text to NetworkResource

--- app/Http/Resources/NetworkResource.php
+++ app/Http/Resources/NetworkResource.php
@@ -22,6 +22,13 @@ class NetworkResource extends JsonResource
             'search_sublabel' => $this->search_sublabel,
             'network_label'   => $this->network_label,
             'browse_label'    => $this->browse_label,
+            'legal'           => [
+                'home'     => $this->legal_home,
+                'search'   => $this->legal_search,
+                'browse'   => $this->legal_browse,
+                'provider' => $this->legal_provider,
+                'facility' => $this->legal_facility,
+            ],
         ];
     }
 }
