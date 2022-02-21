30f0403a7e10cd0617917dc2860f3c7fdf33d9a4
Pablo de la Peña <p@shedcollective.org>
2022-02-21 15:26:25 +0100

    feat: Console output with ability to retrieve log entries

---
+++ app/Services/DataSource/Console/Output.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services\DataSource\Console;
+
+use Illuminate\Console\OutputStyle;
+
+class Output extends OutputStyle
+{
+    private array $log = [];
+
+    public function write($messages, bool $newline = false, int $type = self::OUTPUT_NORMAL)
+    {
+        $this->logMessages($messages);
+        parent::write($messages, $newline, $type);
+    }
+
+    public function writeln($messages, int $type = self::OUTPUT_NORMAL)
+    {
+        $this->logMessages($messages);
+        parent::writeln($messages, $type);
+    }
+
+    private function logMessages($messages): void
+    {
+        if (!is_iterable($messages)) {
+            $messages = [$messages];
+        }
+
+        foreach ($messages as $message) {
+            $this->log[] = $message;
+        }
+    }
+
+    public function getLog(): array
+    {
+        return $this->log;
+    }
+}
