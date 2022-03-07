3f8c50ae163f25275bd8f1451b4c6cd25fb036fa
Pablo de la Peña <p@shedcollective.org>
2022-03-07 11:01:58 +0000

    chore: Support additional phone formatting

--- app/Helper/Formatter.php
+++ app/Helper/Formatter.php
@@ -8,7 +8,11 @@ final class Formatter
     {
         if (preg_match('/\d{10,}/', $number)) {
             return preg_replace('/(\d{3,})(\d{3,})(\d{4,})/', '($1) $2-$3', $number);
+
+        } elseif (preg_match('/\(\d{3,}\)\d{3,}-\d{4,}/', $number)) {
+            return preg_replace('/(\d)\)(\d)/', '$1) $2', $number);
         }
+
         return $number;
     }
 }
